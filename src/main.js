ad1dc93a61305031ff34bac15bc7eb75c3b2b484
i_raj3sh <rajesh.rxca@gmail.com>
2020-10-22 10:49:19 +0530

    vuex initial commit after installation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ToDoList from './views/ToDoList.vue'
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -9,8 +10,11 @@ window.eventBus = new Vue();
 
 new Vue({
     router,
+
     components: {
         ToDoList
     },
+
+    store,
     render: h => h(App)
 }).$mount('#app')
